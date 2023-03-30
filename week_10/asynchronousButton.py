626a1e1dc809d7617104a3c93c84e8d1734363df
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2023-03-29 23:32:04 -0400

    Create asynchronousButton.py

---
+++ week_10/asynchronousButton.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+
+LED=18
+BUTTON1 = 24
+BUTTON2 = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(BUTTON1, GPIO.IN, pull_up_down = GPIO.PUD_UP) #default UP
+GPIO.setup(BUTTON2, GPIO.IN, pull_up_down = GPIO.PUD_UP) #default UP
+
+def backdoor(channel):
+    GPIO.output(LED, GPIO.HIGH)   #LED ON
+    print ("Back door")
+    time.sleep(0.1) #on just for a few
+    GPIO.output(LED, GPIO.LOW) # LED OFF
+
+def frontdoor(channel):
+    GPIO.output(LED, GPIO.HIGH)   #LED ON
+    print ("Front door")
+    time.sleep(0.1) #ON just for a few
+    GPIO.output(LED, GPIO.LOW) # LED OFF    
+    
+GPIO.add_event_detect(BUTTON1, GPIO.FALLING, callback=backdoor)
+GPIO.add_event_detect(BUTTON2, GPIO.FALLING, callback=frontdoor)     
+
+try:
+  while True:
+      pass
+except KeyboardInterrupt:
+  GPIO.cleanup()
+
+print("End of the program")
