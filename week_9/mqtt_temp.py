62cdd0308e307529df937939fa7f93a45201d476
gabrielastudillo <gastudil@espol.edu.ec>
2025-03-25 12:11:02 -0400

    added the mqtt temp file

--- week_9/mqtt_temp.py
+++ week_9/mqtt_temp.py
@@ -1,1 +1,26 @@
+from gpiozero import LED
+import time
+import random
+import paho.mqtt.client as mqtt
 
+red = LED(17)
+id = '67e6ce98-537b-414e-871f-f0cbc009ee63'
+client_name = id + 'temperature_client'
+
+mqtt_client = mqtt.Client(client_name)
+mqtt_client.connect('test.mosquitto.org')
+
+mqtt_client.loop_start()
+
+print("MQTT connected!")
+
+while True:
+    temperature = random.randrange(23,27)
+    print('Light level:', temperature)
+
+    if temperature > 24:
+        red.on()
+    else:
+        red.off()
+    
+    time.sleep(3)
